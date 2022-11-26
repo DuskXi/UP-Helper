f3bbe13e873b1eaa5eab29d0814072c16dc86c25
duskxi <84715902+duskxi@users.noreply.github.com>
2022-11-26 22:37:12 +0000

    搜索页脚本加载器

---
+++ src-bex/scripts/components/SearchingPage.js
@@ -0,0 +1,16 @@
+import {Component} from '../components';
+
+export class SearchingPage extends Component {
+  constructor(tabManager) {
+    super("SearchingPage");
+    this.tabManager = tabManager;
+  }
+
+  init() {
+    super.init();
+  }
+
+  destroy() {
+    super.destroy();
+  }
+}
